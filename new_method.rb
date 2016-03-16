714e2ee189e9ade86a5ccec82bcf5b2058dfd376
Derek Cover <dcover@mesalabs.com>
2016-03-16 07:44:25 -0600

    First early lessons

--- new_method.rb
+++ new_method.rb
@@ -1,5 +1,10 @@
-def sum_int (numbers)
-  numbers +7
+def sum_int(num1, num2)
+  num1 + num2
 end
 
-puts sum_int(3)
+puts 'Enter the first number'
+num1 = gets.chomp.to_i
+
+puts 'Enter the second number'
+num2 = gets.chomp.to_i
+puts "The sum is: #{sum_int(num1, num2)}"
