df456e732b1bd55ce9828695a723db54d54dcf6b
Derek Cover <dcover@mesalabs.com>
2016-03-21 21:15:38 -0600

    lesson material

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -4,14 +4,16 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("Congratulations, and now there will be cake. (win)")
-  elsif (player == 'scissors' && computer == 'rock') ||
-        (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors')
+  elsif win?(computer, player)
     prompt("The cake is all mine. (loss)")
   else
     prompt("Looks like neither one of us get cake. (tie)")
