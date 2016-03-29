62fc4b9280d150bcdf6e6059fc6577c84af5bac6
Derek Cover <dcover@mesalabs.com>
2016-03-29 14:56:23 -0600

    Reworked repo to Lessons and create subdirectories to individual lessons

---
+++ lesson_2/test.rb
@@ -0,0 +1,6 @@
+some_variable=5
+if some_variable == 2
+  puts "some variable is 2"
+else
+  puts "some variable is not 2"
+end
---
+++ lesson_3/famous_words.rb
@@ -0,0 +1,11 @@
+famous_words = "seven years ago..."
+
+famous_words.prepend("Four score and ")
+
+puts famous_words
+
+puts "*****************"
+
+famous_words = "seven years ago..."
+
+puts "Four score and " << famous_words
