4eabbd6effbf3a05499d45154bb437630e52d3ea
Derek Cover <dcover@mesalabs.com>
2016-04-01 14:24:48 -0600

    Reworked repo to Lessons and create subdirectories to individual lessons

--- lesson_3/flintstones_easy3.rb
+++ lesson_3/flintstones_easy3.rb
@@ -37,4 +37,17 @@ puts title.center(40)
 
 puts "*****"
 
-puts 10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
+10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
+
+puts "*****"
+
+statement = "The Flintstones Rock"
+
+result = {}
+letters = ('A'..'Z').to_a + ('a'..'z').to_a
+letters.each do |letter|
+  letter_frequency = statement.scan(letter).count
+  result[letter] = letter_frequency if letter_frequency > 0
+end
+
+puts result
