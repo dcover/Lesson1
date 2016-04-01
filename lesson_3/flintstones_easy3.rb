1380a895cb9a251b691f8fea2c2d3ae2388e3870
Derek Cover <dcover@mesalabs.com>
2016-04-01 13:48:48 -0600

    Reworked repo to Lessons and create subdirectories to individual lessons

---
+++ lesson_3/flintstones_easy3.rb
@@ -0,0 +1,40 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+puts flintstones
+
+puts "*****"
+
+flintstones = %w(Fred Barney Wilman Better BamBam Pebbles)
+puts flintstones
+
+puts "*****"
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << "Dino"
+
+puts flintstones
+
+puts "*****"
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.push("Dino").push("Hoppy")
+
+puts flintstones
+
+puts "*****"
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+puts advice.slice!(0, advice.index('house'))
+
+puts "*****"
+
+statement = "The Flintstones Rock!"
+puts statement.scan('t').count
+
+puts "*****"
+
+title = "Flintstone Family Members"
+puts title.center(40)
+
+puts "*****"
+
+puts 10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
